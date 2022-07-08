20fac11d98f5aa9d0285184065bc08b5eebb2d80
Jermaine Walters <walte1jj@gmail.com>
2022-07-08 10:31:13 -0400

    class to function

---
+++ client/src/components/CreateWorkout/CreateWorkout.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
+
+// export default function CreateWorkout() {
+
+// }
+
+
---
+++ client/src/components/Workout/Workout.js
@@ -0,0 +1,15 @@
+export default function Workout(props) {
+    return (
+        <tr>
+        <td>{ props.workout.username }</td>
+        <td>{ props.workout.description }</td>
+        <td>{ props.workout.duration }</td>
+        <td>{ props.workout.date.substring(0, 10) }</td>
+        <td>
+            <Link to={ "/edit/" + props.workout._id }>Edit</Link>
+            <a href="#" onClick={() => props.deleteWorkout(props.workout._id)}>Delete</a>
+        </td>
+        </tr>
+    )
+    
+}
