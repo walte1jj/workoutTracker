10f1909d59f20c4bd8e7da8985dd54803e33178e
Matt Habbo <matthabbo45@gmail.com>
2022-07-11 18:39:12 -0400

    updated user routes, unable to test

--- server/models/User.js
+++ server/models/User.js
@@ -20,8 +20,34 @@ const userSchema = new Schema(
             required: true,
             minlength: 5
         }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        }
     }
 );
+
+
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+  
+
+  userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+  };
+  
+
+  userSchema.virtual('bookCount').get(function () {
+    return this.savedBooks.length;
+  });
+  
 
 const User = model('User', userSchema);
 
