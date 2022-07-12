419b316f553ca528627dff428c6658f94513a5bb
Matt Habbo <matthabbo45@gmail.com>
2022-07-12 19:42:49 -0400

    updated exercise route

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const exerciseRoutes = require('./exercise-routes');
 
 router.use('/users', userRoutes);
+router.use('/exercise', exerciseRoutes);
 
 module.exports = router;
