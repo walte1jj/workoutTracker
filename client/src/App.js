c9efb8b28bdf69f7f440624a4b99c6d756d08a9d
Jermaine Walters <walte1jj@gmail.com>
2022-07-13 15:17:53 -0400

    merge attempt 56

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import Register from './components/Register';
+import Login from './components/Login';
+// import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <Router>
-      <Navbar />
+    <main className="App">
+      <Register />
+      <Login />
+      {/* <Router> */}
+      {/* <Navbar /> */}
       <br />
       <div classname = "container">
-        <Route path = "/" exact component = { WorkoutList } />
+        {/* <Route path = "/" exact component = { WorkoutList } />
         <Route path = "/edit/:id" component = { EditWorkout } />
         <Route path = "/create" component = { CreateWorkout } />
-        <Route path = "/user" component = { CreateUser } />
+        <Route path = "/user" component = { CreateUser } /> */}
       </div>
-    </Router>
+    {/* </Router> */}
+    </main>
   );
 }
 
