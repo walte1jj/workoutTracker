678cb6804bdf23405cecab786beda62d81a002d6
ABlackmon1107 <aireealblackmon@gmail.com>
2022-07-13 22:22:52 -0400

    nav

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -12,17 +12,17 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="#home">Workout Tracker</Nav.Link>
+            <Nav.Link href="#link">Create User</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Create Workout Log</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Workouts</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
-                Separated link
+                Workout List
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
